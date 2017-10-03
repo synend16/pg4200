b21ccaa4c628cb0d8136254fc996cdb851ff2ca7
synend16 <synend16@student.westerdals.no>
2017-10-03 10:13:21 +0200

    Øving 3 ferdig, skal begynne på øving 4

--- src/main/endre/ex04/MixedSort.java
+++ src/main/endre/ex04/MixedSort.java
@@ -22,76 +22,7 @@ public class MixedSort implements MySort {
     }
 
 
-    private <T extends Comparable<T>> void mergeSort(int low, int high, T[] array, T[] buffer){
-
-        if (high - low < bubbleLimit){
-            bubbleSort(low, high, array);
-            return;
-        }
-
-        if (low < high){
-            int middle = low + (high - low) / 2;
-
-            mergeSort(low, middle, array, buffer);
-
-            mergeSort(middle + 1, high, array, buffer);
-
-            merge(low, middle, high, array, buffer);
-        }
-
-
-    }
-
-    private <T extends Comparable<T>> void bubbleSort(int low, int high, T[] array){
-        boolean swapped = true;
-        int lastSwap = high;
-
-        while (swapped){
-            swapped = false;
-            int limit = lastSwap;
-
-            for (int i = low; i < limit; i++) {
-                int j = i + 1;
-
-                if (array[i].compareTo(array[j]) > 0){
-                    T tmpValue = array[i];
-                    array[i] = array[j];
-                    array[j] = tmpValue;
-
-                    swapped = true;
-                    lastSwap = i;
-                }
-            }
-        }
-    }
-
-
-    private <T extends Comparable<T>> void merge(int low, int middle, int high, T[]array, T[]buffer){
-        for (int i = low; i <= high; i++) {
-            buffer[i] = array[i];
-        }
-
-        int i = low;
-        int j = middle + 1;
-        int k = low;
-
-        while (i <= middle && j <= high){
-            if (buffer[i].compareTo(buffer[j]) <= 0){
-                array[k] = buffer[i];
-                i++;
-            } else {
-                array[k] = buffer[i];
-                j++;
-            }
-            k++;
-        }
-
-        while (i <= middle){
-            array[k] = buffer[i];
-            k++;
-            i++;
-        }
-
+    private <T extends Comparable<T>> void mergeSort(int low, int high, T[]array, T[] buffer){
 
     }
 }
